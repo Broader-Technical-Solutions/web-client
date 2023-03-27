e0ecc9c091e18544daf5e94307861119de0c2a69
Grant Broadwater <grant.broadwater@garmin.com>
2023-03-27 10:54:23 -0500

    Add MDL Library
    Add the mdl library for UI elements.
    
    Change-Id: I81e8034899a5165195ef2d01d157fec1d0dd19c0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+export default function Index() {
   return (
     <>
       <Head>
         <title>Web Client</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <h1>Hello, world!</h1>
+
+        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+          Button
+        </button>
+
       </main>
-    </>
+
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css" />
+      <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+      </>
   )
 }
